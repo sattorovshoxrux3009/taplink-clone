1a2b3305c93adc4c86de344b89947cacd4137a9a
sattorovshoxrux3009 <sattorovshoxrux3009@gmail.com>
2025-04-20 02:09:00 +0500

    create project

---
+++ src/App.jsx
@@ -0,0 +1,415 @@
+import "./App.scss";
+import logo from "../src/img/logo.jpg";
+
+function App() {
+  return (
+    <>
+      <div className="container">
+        <div className="logo-wrapper">
+          <img className="logo" src={logo} alt="" />
+        </div>
+        <h1>Promokod.uz</h1>
+        <h6>Доимий Янги Промокодлар ва Чегирмалар бизда!</h6>
+        <ul>
+          <a href="https://t.me/yandeks_telegram/76" target="_blank">
+            <li>
+              <svg
+                role="none"
+                data-mode="stroke"
+                xmlns="http://www.w3.org/2000/svg"
+                width="24"
+                height="24"
+                viewBox="0 0 24 24"
+                stroke="#fff"
+                fill="none"
+              >
+                <circle cx="7" cy="17" r="2"></circle>
+                <circle cx="17" cy="17" r="2"></circle>
+                <path d="M5 17H3V6a1 1 0 0 1 1-1h9v6H8l2 2m0-4l-2 2m1 6h6M13 6h5l3 5v6h-2"></path>
+              </svg>
+              <h2 className="title">Uzum market</h2>
+              <p className="text">Vaqtinchalik pauza!</p>
+            </li>
+          </a>
+          <a href="https://t.me/yandeks_telegram/298" target="_blank">
+            <li>
+              <svg
+                role="none"
+                data-mode="stroke"
+                xmlns="http://www.w3.org/2000/svg"
+                width="24"
+                height="24"
+                viewBox="0 0 24 24"
+                stroke-width="1"
+                stroke="#000"
+                fill="none"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+              >
+                <circle cx="6" cy="19" r="2"></circle>
+                <circle cx="17" cy="19" r="2"></circle>
+                <path d="M17 17H6V3H4"></path>
+                <path d="M20 6l-1 7H6m4-3l6-6"></path>
+                <circle cx="10.5" cy="4.5" r=".5"></circle>
+                <circle cx="15.5" cy="9.5" r=".5"></circle>
+              </svg>
+              <h2 className="title">Yandex market</h2>
+              <p className="text">Promokod - </p>
+            </li>
+          </a>{" "}
+          <hr className="line" />
+          <a href="https://t.me/yandeks_telegram/299" target="_blank">
+            <li>
+              <svg
+                role="none"
+                data-mode="stroke"
+                xmlns="http://www.w3.org/2000/svg"
+                width="24"
+                height="24"
+                viewBox="0 0 24 24"
+                stroke-width="1"
+                stroke="#fff"
+                fill="none"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+              >
+                <circle cx="18" cy="17" r="2"></circle>
+                <path d="M5 16v1a2 2 0 0 0 4 0v-5H6a3 3 0 0 0-3 3v1h10a6 6 0 0 1 5-4V7a2 2 0 0 0-2-2h-1"></path>
+                <path d="M6 9h3"></path>
+              </svg>
+              <h2 className="title">Uzum tezkor</h2>
+              <p className="text">Promokod - YXLTD</p>
+            </li>
+          </a>{" "}
+          <a href="https://t.me/yandeks_telegram/300" target="_blank">
+            <li>
+              <svg
+                role="none"
+                data-mode="stroke"
+                xmlns="http://www.w3.org/2000/svg"
+                width="24"
+                height="24"
+                viewBox="0 0 24 24"
+                stroke-width="1"
+                stroke="#000"
+                fill="none"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+              >
+                <circle cx="18" cy="17" r="2"></circle>
+                <path d="M5 16v1a2 2 0 0 0 4 0v-5H6a3 3 0 0 0-3 3v1h10a6 6 0 0 1 5-4V7a2 2 0 0 0-2-2h-1"></path>
+                <path d="M6 9h3"></path>
+              </svg>
+              <h2 className="title">Yandex go (EDA)</h2>
+              <p className="text">Promokod - UKMKH</p>
+            </li>
+          </a>{" "}
+          <a href="https://t.me/yandeks_telegram/301" target="_blank">
+            <li>
+              <svg
+                role="none"
+                data-mode="stroke"
+                xmlns="http://www.w3.org/2000/svg"
+                width="24"
+                height="24"
+                viewBox="0 0 24 24"
+                stroke-width="1"
+                stroke="#000"
+                fill="none"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+              >
+                <circle cx="18" cy="17" r="2"></circle>
+                <path d="M5 16v1a2 2 0 0 0 4 0v-5H6a3 3 0 0 0-3 3v1h10a6 6 0 0 1 5-4V7a2 2 0 0 0-2-2h-1"></path>
+                <path d="M6 9h3"></path>
+              </svg>
+              <h2 className="title">Express 24</h2>
+              <p className="text">Promokod - EKE9S</p>
+            </li>
+          </a>{" "}
+          <a href="https://t.me/yandeks_telegram/302" target="_blank">
+            <li>
+              <svg
+                role="none"
+                data-mode="stroke"
+                xmlns="http://www.w3.org/2000/svg"
+                width="24"
+                height="24"
+                viewBox="0 0 24 24"
+                stroke-width="1"
+                stroke="#fff"
+                fill="none"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+              >
+                <circle cx="18" cy="17" r="2"></circle>
+                <path d="M5 16v1a2 2 0 0 0 4 0v-5H6a3 3 0 0 0-3 3v1h10a6 6 0 0 1 5-4V7a2 2 0 0 0-2-2h-1"></path>
+                <path d="M6 9h3"></path>
+              </svg>
+              <h2 className="title">Oqtepa lavash bot</h2>
+              <p className="text">Promokod - CHEGIRMA30</p>
+            </li>
+          </a>{" "}
+          <a href="https://t.me/yandeks_telegram/309" target="_blank">
+            <li>
+              <svg
+                role="none"
+                data-mode="stroke"
+                xmlns="http://www.w3.org/2000/svg"
+                width="24"
+                height="24"
+                viewBox="0 0 24 24"
+                stroke-width="1"
+                stroke="#fff"
+                fill="none"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+              >
+                <circle cx="18" cy="17" r="2"></circle>
+                <path d="M5 16v1a2 2 0 0 0 4 0v-5H6a3 3 0 0 0-3 3v1h10a6 6 0 0 1 5-4V7a2 2 0 0 0-2-2h-1"></path>
+                <path d="M6 9h3"></path>
+              </svg>
+              <h2 className="title">Milly</h2>
+              <p className="text">Tez orada!</p>
+            </li>
+          </a>{" "}
+          <a href="https://t.me/yandeks_telegram/308" target="_blank">
+            <li>
+              <svg
+                role="none"
+                data-mode="stroke"
+                xmlns="http://www.w3.org/2000/svg"
+                width="24"
+                height="24"
+                viewBox="0 0 24 24"
+                stroke-width="1"
+                stroke="#fff"
+                fill="none"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+              >
+                <circle cx="18" cy="17" r="2"></circle>
+                <path d="M5 16v1a2 2 0 0 0 4 0v-5H6a3 3 0 0 0-3 3v1h10a6 6 0 0 1 5-4V7a2 2 0 0 0-2-2h-1"></path>
+                <path d="M6 9h3"></path>
+              </svg>
+              <h2 className="title">Wolt</h2>
+              <p className="text">Tez orada!</p>
+            </li>
+          </a>{" "}
+          <hr className="line" />
+          <a
+            href="https://ypls-eats.prfl.me/promokod.chegirmalar/ki8oby"
+            target="_blank"
+          >
+            <li>
+              <svg
+                role="none"
+                data-mode="stroke"
+                xmlns="http://www.w3.org/2000/svg"
+                width="24"
+                height="24"
+                viewBox="0 0 24 24"
+                stroke-width="1"
+                stroke="#fff"
+                fill="none"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+              >
+                <rect
+                  x="4"
+                  y="13"
+                  rx="2"
+                  width="4"
+                  height="6"
+                  viewBox="0 0 4 6"
+                ></rect>
+                <rect
+                  x="16"
+                  y="13"
+                  rx="2"
+                  width="4"
+                  height="6"
+                  viewBox="0 0 4 6"
+                ></rect>
+                <path d="M4 15v-3a8 8 0 0 1 16 0v3m-2 4a6 3 0 0 1-6 3"></path>
+              </svg>
+              <h2 className="title">Yandex plus</h2>
+              <p className="text">Promokod - 2UE8DDBES8</p>
+            </li>
+          </a>{" "}
+          <a href="https://t.me/yandeks_telegram/307" target="_blank">
+            <li>
+              <svg
+                role="none"
+                data-mode="stroke"
+                xmlns="http://www.w3.org/2000/svg"
+                width="24"
+                height="24"
+                viewBox="0 0 24 24"
+                stroke-width="1"
+                stroke="#000"
+                fill="none"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+              >
+                <circle cx="7" cy="17" r="2"></circle>
+                <circle cx="17" cy="17" r="2"></circle>
+                <path d="M5 17H3V6a1 1 0 0 1 1-1h9v12m-4 0h6m4 0h2v-6h-8m0-5h5l3 5"></path>
+              </svg>
+              <h2 className="title">Yandex dostavka</h2>
+              <p className="text">Tez orada!</p>
+            </li>
+          </a>{" "}
+          <a href="https://t.me/yandeks_telegram/305" target="_blank">
+            <li>
+              <svg
+                role="none"
+                data-mode="stroke"
+                xmlns="http://www.w3.org/2000/svg"
+                width="24"
+                height="24"
+                viewBox="0 0 24 24"
+                stroke-width="1"
+                stroke="#000"
+                fill="none"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+              >
+                <circle cx="7" cy="17" r="2"></circle>
+                <circle cx="17" cy="17" r="2"></circle>
+                <path d="M5 17H3V6a1 1 0 0 1 1-1h9v12m-4 0h6m4 0h2v-6h-8m0-5h5l3 5"></path>
+              </svg>
+              <h2 className="title">Yandex magazin</h2>
+              <p className="text">Promokod - UKJH3</p>
+            </li>
+          </a>{" "}
+          <a href="https://t.me/yandeks_telegram/306" target="_blank">
+            <li>
+              <svg
+                role="none"
+                data-mode="stroke"
+                xmlns="http://www.w3.org/2000/svg"
+                width="24"
+                height="24"
+                viewBox="0 0 24 24"
+                stroke-width="1"
+                stroke="#fff"
+                fill="none"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+              >
+                <circle cx="18" cy="17" r="2"></circle>
+                <path d="M5 16v1a2 2 0 0 0 4 0v-5H6a3 3 0 0 0-3 3v1h10a6 6 0 0 1 5-4V7a2 2 0 0 0-2-2h-1"></path>
+                <path d="M6 9h3"></path>
+              </svg>
+              <h2 className="title">Yandex lavka</h2>
+              <p className="text">Tez orada!</p>
+            </li>
+          </a>{" "}
+          <span>
+            <div className="line1"></div>
+            <div className="box"></div>
+            <div className="line2"></div>
+          </span>
+          <a href="https://t.me/yandeks_telegram" target="_blank">
+            <li>
+              <svg
+                className="my-icon"
+                role="none"
+                data-mode="fill"
+                xmlns="http://www.w3.org/2000/svg"
+                width="32"
+                height="32"
+                viewBox="0 0 32 32"
+              >
+                <path d="M16 1.984C8.256 1.984 2.016 8.256 2.016 16S8.256 30.016 16 30.016 30.016 23.744 30.016 16 23.744 1.984 16 1.984zm4.32 20.672s-.352.864-1.312.448l-3.584-2.72-2.176-1.728v-.064l6.656-6.016.192-.288c0-.288-.48 0-.48 0l-8.48 5.376-3.52-1.184s-.544-.192-.608-.608.608-.64.608-.64l14.048-5.504s1.152-.512 1.152.32L20.32 22.656z"></path>
+              </svg>
+              <h2 className="title">Telegram kanal</h2>
+            </li>
+          </a>{" "}
+          <a href="https://t.me/promokodlar_uzb_bot" target="_blank">
+            <li>
+              <svg
+                className="my-icon"
+                role="none"
+                data-mode="fill"
+                xmlns="http://www.w3.org/2000/svg"
+                width="32"
+                height="32"
+                viewBox="0 0 32 32"
+              >
+                <path d="M16 1.984C8.256 1.984 2.016 8.256 2.016 16S8.256 30.016 16 30.016 30.016 23.744 30.016 16 23.744 1.984 16 1.984zm4.32 20.672s-.352.864-1.312.448l-3.584-2.72-2.176-1.728v-.064l6.656-6.016.192-.288c0-.288-.48 0-.48 0l-8.48 5.376-3.52-1.184s-.544-.192-.608-.608.608-.64.608-.64l14.048-5.504s1.152-.512 1.152.32L20.32 22.656z"></path>
+              </svg>
+              <h2 className="title">Telegram bot</h2>
+            </li>
+          </a>{" "}
+          <a
+            href="https://www.youtube.com/@AbdugafforRaxmatjonov-wc3ky"
+            target="_blank"
+          >
+            <li>
+              <svg
+                role="none"
+                data-mode="fill"
+                xmlns="http://www.w3.org/2000/svg"
+                width="24"
+                height="24"
+                fill="none"
+                stroke="currentColor"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                viewBox="0 0 24 24"
+              >
+                <path
+                  fill="currentColor"
+                  stroke="none"
+                  d="M18 3a5 5 0 0 1 5 5v8a5 5 0 0 1-5 5H6a5 5 0 0 1-5-5V8a5 5 0 0 1 5-5zM9 9v6a1 1 0 0 0 1.514.857l5-3a1 1 0 0 0 0-1.714l-5-3A1 1 0 0 0 9 9"
+                ></path>
+              </svg>
+              <h2 className="title">Youtube kanal</h2>
+            </li>
+          </a>{" "}
+          <a
+            href="https://www.instagram.com/yandexgo.promokod?igsh=MWE1MjB3aThpaGI1dA%3D%3D&utm_source=qr"
+            target="_blank"
+          >
+            <li>
+              <svg
+                className="my-icon"
+                role="none"
+                data-mode="fill"
+                xmlns="http://www.w3.org/2000/svg"
+                width="32"
+                height="32"
+                viewBox="0 0 32 32"
+              >
+                <path d="M15.968 7.776c-4.512 0-8.224 3.68-8.224 8.192 0 4.544 3.712 8.224 8.224 8.224 4.544 0 8.224-3.68 8.224-8.224 0-4.512-3.68-8.192-8.224-8.192zm0 13.28c-2.784 0-5.088-2.272-5.088-5.088 0-2.784 2.304-5.056 5.088-5.056 2.816 0 5.088 2.272 5.088 5.056a5.08 5.08 0 0 1-5.088 5.088zM22.688.16H9.248C4.224.16.16 4.256.16 9.28v13.408c0 5.024 4.064 9.12 9.088 9.12h13.44a9.14 9.14 0 0 0 9.12-9.12V9.28a9.14 9.14 0 0 0-9.12-9.12zm5.984 22.528a6 6 0 0 1-5.984 5.984H9.248c-3.296 0-5.952-2.688-5.952-5.984V9.28c0-3.296 2.656-5.984 5.952-5.984h13.44a6 6 0 0 1 5.984 5.984v13.408zM24.32 5.376a2.2 2.2 0 0 0-2.176 2.176c0 1.184.96 2.144 2.176 2.144 1.184 0 2.144-.96 2.144-2.144s-.96-2.176-2.144-2.176z"></path>
+              </svg>
+              <h2 className="title">Instagram kanal</h2>
+            </li>
+          </a>
+          <a href="https://t.me/tuit_01" target="_blank">
+            <li>
+              <svg
+                className="my-icon"
+                role="none"
+                data-mode="fill"
+                xmlns="http://www.w3.org/2000/svg"
+                width="32"
+                height="32"
+                viewBox="0 0 32 32"
+              >
+                <path d="M29.056 25.696c-.064-1.312-.416-2.432-1.664-3.072-.352-.192-.736-.384-1.088-.512l-4.32-1.728c-.704-.288-.736-.256-.928.448l-2.72 10.24c-.064.192-.064.384-.224.576l-.096-.384-.576-5.12c-.16-1.472-.576-2.944.032-4.416.128-.224-.064-.256-.256-.256h-2.24c-.416 0-.576.064-.416.48.288.8.384 1.632.224 2.464-.128.672-.192 1.376-.288 2.08l-.608 5.12h-.096a1.56 1.56 0 0 0-.096-.224l-2.784-10.496c-.224-.736-.288-.8-1.024-.512L5.664 22.08a6.49 6.49 0 0 0-1.76 1.024c-.512.448-.832.992-.896 1.696l-.544 5.568C2.368 31.488 2.88 32 3.968 32h24.128c.992 0 1.536-.608 1.44-1.536l-.48-4.768zM9.12 13.088c.192.32.416.608.768.704.128 0 .128.096.16.16.256.96.672 1.856 1.28 2.656.896 1.184 2.016 2.08 3.488 2.432.256.064.512.096.736.128h.896l.896-.16c1.152-.32 2.112-.992 2.912-1.888.832-.896 1.376-1.952 1.696-3.136a.27.27 0 0 1 .224-.224c.416-.16.672-.512.8-.928.16-.448.384-.896.48-1.376v-.544c-.032-.192-.096-.384-.16-.576-.096-.288-.256-.544-.544-.704-.064-.032-.064-.096-.032-.16.512-1.12.416-2.304.288-3.488-.096-.8-.288-1.6-.544-2.368-.192-.672-.608-1.216-1.12-1.664-.8-.64-1.728-1.12-2.72-1.376-.48-.128-.928-.032-1.344.224l-.384.288c-.16-.672-.736-.8-1.248-.928L14.752 0h-.896c-.16.064-.352.064-.512.096-.512.096-.96.32-1.376.672-1.024.864-1.856 1.888-2.4 3.136-.384.928-.512 1.92-.64 2.912-.096.896.032 1.76.32 2.592.032.128.032.192-.096.256-.352.256-.512.64-.576 1.056-.128.576 0 1.088.224 1.6.128.224.16.512.32.768zm.192-2.752c.128-.224.192-.224.288 0 .192.352.512.576.832.8-.192-1.728.064-3.392.192-5.056.192.256.352.544.608.832-.128-.512-.256-.992-.192-1.504.16.256.224.48.448.704-.064-.256-.096-.48-.064-.704.064-.736.48-1.184 1.152-1.44.672-.224 1.344-.288 2.016-.064.896.288 1.728.224 2.592-.064.384-.128.768-.224 1.152-.16.672.128 1.12.544 1.408 1.12.192.32.288.672.512.96.16.288.384.48.672.608.32.128.352.192.352.544-.032.16-.032.32-.064.48-.064.352-.032.704.16 1.024.128.288.224.544.384.8.128.256.16.544.128.8l-.064 1.216c.224-.352.384-.672.544-.992.064-.128.128-.128.224-.032.128.16.192.352.256.544.064.448.032.896-.192 1.312-.096.192-.16.384-.224.608-.064.128-.128.256-.224.384-.128.128-.288.224-.48.16-.128-.032-.16.064-.192.16-.224.896-.544 1.728-1.056 2.496-.864 1.376-2.048 2.336-3.68 2.656-1.248.224-2.4-.064-3.456-.768-1.44-.992-2.272-2.368-2.752-4-.032-.064-.032-.128-.064-.192-.032-.096-.032-.224-.096-.32s-.192 0-.288 0c-.192-.032-.352-.128-.416-.288-.256-.544-.544-1.088-.608-1.696-.032-.352.032-.672.192-.928z"></path>
+              </svg>
+              <h2 className="title">Напишите мне</h2>
+              <p className="text">Abdug'affor Raxmatjonov</p>
+            </li>
+          </a>
+        </ul>
+        <p className="end-text">+998 (90) 783-83-00</p>
+      </div>
+    </>
+  );
+}
+
+export default App;
